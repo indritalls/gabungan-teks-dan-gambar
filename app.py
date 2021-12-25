702a75bdbfd4ae75d6dcd5e995f18dbc4042e6d2
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-25 15:57:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -198,5 +198,7 @@ def handle_message(event):
             sticker_id=stiker)
         line_bot_api.reply_message(event.reply_token, sticker_message)
 
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
