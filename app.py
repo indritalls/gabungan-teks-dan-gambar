5f01694dff73081ef695a7909fc567febb94d216
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-29 14:52:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from linebot.models import (
     MessageEvent, TextMessage, FlexSendMessage, 
     TemplateSendMessage, ConfirmTemplate, PostbackTemplateAction, MessageTemplateAction,
     ButtonsTemplate, URITemplateAction, TextSendMessage, CarouselTemplate, CarouselColumn, ImageSendMessage, StickerSendMessage,
-    ImageCarouselTemplate, ImageCarouselColumn
+    RichMenu, RichMenuSize, RichMenuArea, RichMenuBounds, MessageAction, config
 )
 
 app = Flask(__name__)
@@ -197,6 +197,45 @@ def handle_message(event):
             package_id='11537',
             sticker_id=stiker)
         line_bot_api.reply_message(event.reply_token, sticker_message)
+
+def createRichmenu():
+    result = False
+    try:
+        # define a new richmenu
+        rich_menu_to_create = RichMenu(
+            size = RichMenuSize(width=1200, height=405),
+            selected = True,
+            name = 'richmenu for randomchat',
+            chat_bar_text = 'TAP HERE',
+            areas=[
+                RichMenuArea(
+                    bounds=RichMenuBounds(x=0, y=0, width=480, height=405),
+                    action=MessageAction(text=config.REMOVE)
+                ),
+                RichMenuArea(
+                    bounds=RichMenuBounds(x=480, y=0, width=720, height=405),
+                    action=MessageAction(text=config.NEXT)
+                )
+            ]
+        )
+        richMenuId = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+
+        # upload an image for rich menu
+        path = 'image path for richmenu'
+
+        with open(path, 'rb') as f:
+            line_bot_api.set_rich_menu_image(richMenuId, "image/jpeg", f)
+
+        # set the default rich menu
+        line_bot_api.set_default_rich_menu(richMenuId)
+
+        result = True
+
+    except Exception:
+        result = False
+
+
+    return result
 
 import os
 if __name__ == "__main__":
